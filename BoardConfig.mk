e39b136444a29de1718966ad58a35c2b13e36d76
Troj80 <Troj80@users.noreply.github.com>
2021-05-18 15:55:46 +0300

    I'm UMI

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/xiaomi/phoenix
-
+DEVICE_PATH := device/xiaomi/umi
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
@@ -35,9 +34,10 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := phoenix, phoenixin
+TARGET_OTA_ASSERT_DEVICE := umi
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -52,7 +52,8 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 androidboot.selinux=permissive androidboot.usbconfigfs=true
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.init_fatal_reboot_target=recovery
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
@@ -82,7 +83,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
+PLATFORM_VERSION := 20.1.0
 #Encryption 
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
@@ -96,9 +97,9 @@ BOARD_AVB_ENABLE := true
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 
 # Platform
-TARGET_BOARD_PLATFORM := sm6150
+TARGET_BOARD_PLATFORM := kona
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
-QCOM_BOARD_PLATFORMS += sm6150
+QCOM_BOARD_PLATFORMS += kona
 TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
 QCOM_BOARD_PLATFORMS += $(TARGET_BOARD_PLATFORM)
 
@@ -106,40 +107,47 @@ QCOM_BOARD_PLATFORMS += $(TARGET_BOARD_PLATFORM)
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9126805504
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := product vendor system odm
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := product vendor system system_ext odm
 
 # System as root
 BOARD_ROOT_EXTRA_FOLDERS := bluetooth dsp firmware persist
 BOARD_SUPPRESS_SECURE_ERASE := true
 
-# TWRP Configuration
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-RECOVERY_SDCARD_ON_DATA := true
+# TWRP specific build flags
 TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 1000
-TW_Y_OFFSET := 91
-TW_H_OFFSET := -91
+TW_DEFAULT_BRIGHTNESS := 1200
 TARGET_USES_MKE2FS := true
-TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_TWRPAPP := false
+TW_HAS_EDL_MODE := true
+TW_SUPPORT_INPUT_1_2_HAPTICS := true
 TW_NO_SCREEN_BLANK := true
-
+TW_Y_OFFSET := 88
+TW_H_OFFSET := -88
+TW_DEVICE_VERSION := Alex Troj SHRP A11
+PLATFORM_VERSION := 20.1.0
+#TW_SCREEN_BLANK_ON_BOOT := true
+# TW_NO_SCREEN_BLANK := true
 # TWRP_EVENT_LOGGING := true
-TWRP_INCLUDE_LOGCAT := true
 # TWRP Debug Flags
-TARGET_USES_LOGD := true
+#TWRP_INCLUDE_LOGCAT := true
+#TARGET_USES_LOGD := true
 
 # SHRP Flags
 # Device codename
-SHRP_DEVICE_CODE := phoenix
+SHRP_DEVICE_CODE := umi
 # Path of your SHRP device tree
 SHRP_PATH := device/xiaomi/$(SHRP_DEVICE_CODE)
 # Maintainer name
-SHRP_MAINTAINER := hraj9258
+SHRP_MAINTAINER := Alex Troj
 # Recovery Type (for "About" section only)
 # Default (if not set): N/A
 SHRP_REC_TYPE := SAR
@@ -147,18 +155,14 @@ SHRP_REC_TYPE := SAR
 SHRP_REC := /dev/block/bootdevice/by-name/recovery
 # internal storage path
 SHRP_INTERNAL := /sdcard
-# If your device has an external sdcard
-SHRP_EXTERNAL := /external_sd
 # USB OTG path
 SHRP_OTG := /usb_otg
 # Flashlight: (0 = disable, 1 = enable)
 SHRP_FLASH := 1
-# For notch devices
-SHRP_NOTCH := true
 # SHRP Dark mode, use this flag to have dark theme set by default
 SHRP_DARK := true
 # Do not include the SHRP theming system
 SHRP_LITE := true
 # Addon - Clear Fingerprint (remove fingerprint lock from system)
 SHRP_SKIP_DEFAULT_ADDON_3 := true
-SHRP_OFFICIAL := true
+SHRP_OFFICIAL := false
